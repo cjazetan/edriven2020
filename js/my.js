2c8c1a2be32dba556086dd2cf7a772255b0bcd40
cjazetan <carljaze@gmail.com>
2021-03-06 22:00:01 +0800

    Init commit

---
+++ js/my.js
@@ -0,0 +1,17 @@
+let names = "";
+let array = ["aaa","bbb","ccc","ddd"];
+let index = 0;
+
+while(array[index])
+{
+   names += array[index] + " ";
+    index++;
+}
+console.log(names);
+
+const test3 = () => console.log("Hello World");
+test3();
+
+const addTwo = (n1 = 0,n2 = 0) => n1 + n2;
+const sum = addTwo(1,2);
+console.log(sum);
